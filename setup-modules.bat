3b02b2ed9aaccd1534c8b92c7eb5bd3f09ecfcf3
kuwacom <kuwa.com3@gmail.com>
2024-07-12 16:15:47 +0900

    update: gpuへ対応

--- setup-modules.bat
+++ setup-modules.bat
@@ -1,8 +1,8 @@
 @echo off
 call .\venv\Scripts\activate
 
-set CMAKE_ARGS="-DLLAMA_CUBLAS=on"
+set CMAKE_ARGS="-DLLAMA_CUDA=on"
 set FORCE_CMAKE=1
+pip install llama-cpp-python --upgrade --force-reinstall --no-cache-dir
 
-pip install --upgrade --force-reinstall llama-cpp-python --no-cache-dir
 pip install -r req.txt
--- setup-modules.sh
+++ setup-modules.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 source venv/bin/activate
 
-CMAKE_ARGS="-DLLAMA_CUBLAS=on"
+CMAKE_ARGS="-DLLAMA_CUDA=on"
 FORCE_CMAKE=1
-
-pip install llama-cpp-python --force-reinstall --upgrade --no-cache-dir
+pip install llama-cpp-python --upgrade --force-reinstall --no-cache-dir
 
 pip install -r req.txt
